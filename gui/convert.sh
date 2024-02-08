531e38c1e39057aa1ede180a28483f7ed4da18cb
Yongqiang Cheng <yyc@pc102277.ornl.gov>
2024-02-08 11:05:38 -0500

    remove convert.sh

--- gui/convert.sh
+++
@@ -1,6 +0,0 @@
-pyuic5 inspired.ui -o ui_inspired.py
-pyuic5 oclimax.ui -o ui_oclimax.py
-pyuic5 help_dp.ui -o ui_help_dp.py
-pyuic5 help_dft.ui -o ui_help_dft.py
-pyuic5 help_mlff.ui -o ui_help_mlff.py
-pyuic5 help_oclimax.ui -o ui_help_oclimax.py
